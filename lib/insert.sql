32e98d3cc844c95e6f7aa443bd5e3eb58b884adf
Connor Holland <supersoccergoalie123@gmail.com>
2021-03-01 09:10:34 -0700

    commit tree 1ce25af5f683ff1ef2f7057b7759e5dfb3fe64eb

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 23, "M", "Brown", "nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 14, "M", "Black", "nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 25, "F", "Brown", "nice", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 34, "M", "Black", "aggressive", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 27, "F", "Black", "calm", false);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 12, "F", "Brown", "calm", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 7, "F", "Black", "calm", true);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (null, 12, "M", "Brown", "aggresive", false);
